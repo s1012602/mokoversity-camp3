d626c8868176d29f4b649b881db5602e73af07ce
s1012602 <s1012602@pu.edu.tw>
2013-10-19 15:22:05 +0800

    fix #8

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
 var gameModule=(function()
 {
+
+  var timeoutVar,
+      counter=0; 
+
   function start()
   {
-   var canvas = document.getElementById('game');
-   var ctx = canvas.getContext('2d');
- 
+   
+     var canvas = document.getElementById('game');
+     var ctx = canvas.getContext('2d');
+
      var ballX = Math.floor(Math.random() * 300); // 0..300
      var ballY = Math.floor(Math.random() * 500);
      var ballR = Math.floor(Math.random() * 100);
  
-   canvas.width = 480;
-   canvas.height = 320;
- 
-   ctx.fillStyle = 'black';
-   ctx.beginPath();
-   ctx.arc(ballX, ballY, ballR, 0, Math.PI * 2 , true);
-   ctx.fill();
+    canvas.width = 480;
+    canvas.height = 320;
+    ctx.fillStyle = 'black';
+    ctx.beginPath();
+    ctx.arc(ballX, ballY, ballR, 0, Math.PI * 2 , true);
+    ctx.fill();
+
+    if(counter>=10)
+    {}
+    else
+    {
+      timeoutVar=setTimeout(start,1000);
+      counter++;
+
+      console.log("Counter:"+counter);
+    }
   }
    return{start:start}
 })();
