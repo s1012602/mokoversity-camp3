3d7c46b3e45b825149683067999ff608557d025d
s1012602 <s1012602@pu.edu.tw>
2013-10-20 10:06:26 +0800

    fix #9

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ var gameModule=(function()
 {
 
   var timeoutVar,
-      counter=0; 
-      var canvas = document.getElementById('game');
-     var ctx = canvas.getContext('2d');
+      counter=0,
+      canvas = document.getElementById('game'),
+      ctx = canvas.getContext('2d');
   canvas.width = 480;
   canvas.height = 320;
   
@@ -16,7 +16,7 @@ var gameModule=(function()
      var ballY = Math.floor(Math.random() * 500);
      var ballR = Math.floor(Math.random() * 100);
 
-    ctx.fillStyle = 'black';
+    ctx.fillStyle = 'red';
     ctx.beginPath();
     ctx.arc(ballX, ballY, ballR, 0, Math.PI * 2 , true);
     ctx.fill();
